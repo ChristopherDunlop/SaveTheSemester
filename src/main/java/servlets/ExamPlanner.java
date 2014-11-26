b48d4499d6f6e015837dcb9f35af9bde22573825
txroberts <t.x.roberts@dundee.ac.uk>
2014-11-26 10:47:15 +0000

    Get student into exam planner

--- src/main/java/servlets/ExamPlanner.java
+++ src/main/java/servlets/ExamPlanner.java
@@ -57,7 +57,7 @@ public class ExamPlanner extends HttpServlet {
         sm.setCluster(cluster);
         Student student = sm.getStudent(args[2]);
         Set<String> modules = student.getModules();
-        Iterator<String> iterator = modules.iterator();        
+        Iterator<String> iterator = modules.iterator();
         
         request.setAttribute("Student", student);
         
