5d3502326f52a03107336185626e3b80ae00bece
txroberts <t.x.roberts@dundee.ac.uk>
2014-11-27 16:39:22 +0000

    See the files for a student's module

---
+++ src/main/java/servlets/ModuleFiles.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import com.datastax.driver.core.Cluster;
+import java.io.IOException;
+import java.util.Set;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lib.CassandraHosts;
+import lib.Convertors;
+import models.ModuleModel;
+import stores.ModuleFile;
+
+/**
+ *
+ * @author Tom
+ */
+@WebServlet(name = "ModuleFiles", urlPatterns = {"/ModuleFiles/*"})
+public class ModuleFiles extends HttpServlet {
+    
+    private Cluster cluster;
+    
+    public void init(ServletConfig config) throws ServletException {
+        // TODO Auto-generated method stub
+        cluster = CassandraHosts.getCluster();
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String args[] = Convertors.SplitRequestPath(request);
+        
+        ModuleModel mm = new ModuleModel();
+        mm.setCluster(cluster);
+        Set<ModuleFile> moduleFiles = mm.getModuleFiles(args[2], args[3]);
+        
+        request.setAttribute("moduleFiles", moduleFiles);
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/ModuleFiles.jsp");
+        rd.forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/ModuleFiles.jsp
@@ -0,0 +1,68 @@
+<%-- 
+    Document   : ModuleFiles
+    Created on : 27-Nov-2014, 14:40:12
+    Author     : Tom
+--%>
+
+<%@page import="java.util.Iterator"%>
+<%@page import="stores.ModuleFile"%>
+<%@page import="java.util.Set"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Module Files</title>
+    </head>
+    <body>
+        <%
+            Set<ModuleFile> moduleFiles = (Set<ModuleFile>) request.getAttribute("moduleFiles");
+        %>
+        
+        <%
+            if (moduleFiles == null){
+        %>      <p>No module files found</p>
+        <%
+            }
+            else if (moduleFiles.isEmpty()){
+        %>      <p>No module files found</p>
+        <%
+            }
+            else {
+        %>
+            <table border="1">
+                <tr>
+                    <th>File Name</th>
+                    <th>File Type</th>
+                    <th>Num of Pages</th>
+                    <th>Completed</th>
+                    <th>Date Completed</th>
+                </tr>
+        <%
+                Iterator<ModuleFile> iterator = moduleFiles.iterator();
+
+                while (iterator.hasNext()){
+                    ModuleFile moduleFile = iterator.next();
+        %>
+        <tr>
+            <td><%=moduleFile.getFileName()%></td>
+            <td><%=moduleFile.getFileType()%></td>
+            <td><%=moduleFile.getNumPages()%></td>
+            <td><%=moduleFile.isCompleted()%></td>
+        <%
+            if (moduleFile.isCompleted()){
+        %>      
+            <td></td>
+            <td><%=moduleFile.getDateCompleted()%></td>
+        <%  }
+            else {
+        %>      <td>N/A</td>
+        <%  }
+        %>
+            </tr>
+        <%
+                }
+            }
+        %>
+    </body>
+</html>
